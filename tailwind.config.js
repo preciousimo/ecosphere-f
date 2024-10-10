013505bd97f040953c99f3c1e40a7ae6d27fb086
preciousimo <preciousimoniakemu@gmail.com>
2024-10-10 08:29:46 +0100

    "Added @next/font dependency, updated styles and tailwind config"

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
       colors: {
         primary: {
           light: '#3B82F6', // blue-500
-          DEFAULT: '#2563EB', // blue-600
-          dark: '#1D4ED8', // blue-700
+          DEFAULT: '#2563EB', // blue-600 #1E40AF
+          dark: '#1D4ED8', // blue-700 #1E3A8A
         },
         secondary: {
           light: '#FB923C', // orange-400
